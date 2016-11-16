a0b4cc18be615bedd6e0ec9d69386587b07fcef4
Hannah Brock <brockplusplus@gmail.com>
2016-11-15 22:48:37 -0700

    Add weights to move stack and additional weighting to positions moving through hits.

--- cosmo.py
+++ cosmo.py
@@ -15,19 +15,11 @@ class CosmoBot(BoatBot):
         player = self._pick_board()
         # calculate probability of all open squares having a boat
         board_counts = self._calculate_counts(player, self.game.boards[player])
-        # check if we're targetting
+        # check if we're targetting and pick the move with the highest count
         if player in self.move_stack:
-            moves = self.move_stack[player]
-            while len(moves) > 0:
-                move = moves.pop()
-                if board_counts[move] > 0:
-                    (x,y) = self.index_to_coordinate(move)
-                    self.logger.debug('Using stack move {0}'.format(move))
-                    return (player, (x+1,y+1))
-                else:
-                    self.logger.debug('Move {0} has 0 count'.format(move))
-        else:
-            self.logger.debug('Player {0} is not in move stack'.format(player))
+            best_move = self._best_stack_move(self.move_stack[player], board_counts)
+            if best_move is not None:
+                return (player, best_move)
         # hit the square with the highest count otherwise
         return (player, self._max_count(board_counts))
 
@@ -45,6 +37,26 @@ class CosmoBot(BoatBot):
 
         return player
 
+    def _best_stack_move(self, moves, board_counts):
+        max_score = -1
+        best = -1
+        bad_moves = []
+        for move in moves:
+            if board_counts[move] == 0:
+                bad_moves.append(move)
+            elif board_counts[move] > max_score:
+                best = move
+                max_score = board_counts[move]
+        
+        for move in bad_moves:
+            moves.remove(move)
+
+        if best == -1:
+            return None
+        moves.remove(best)
+        (x,y) = self.index_to_coordinate(best)
+        return (x+1, y+1)
+
     def _max_count(self, board_counts):
         max_count = -1
         i = 0
@@ -59,7 +71,7 @@ class CosmoBot(BoatBot):
                     i = ii
                     max_count = board_counts[ii]
 
-        x, y = self.index_to_coordinate(i)
+        (x, y) = self.index_to_coordinate(i)
         return (x+1, y+1) # board index starts at 1
 
     def _calculate_counts(self, player, board):
@@ -69,35 +81,43 @@ class CosmoBot(BoatBot):
             # find all spots to put this boat and increment counts for those squares
             for i in range(0, len(board)):
                 # loop by rows, alternate parity in each row
-                (horizontal, vertical) = self._check_boat_location(board, i, size)
+                (horizontal, hweight, vertical, vweight) = self._check_boat_location(board, i, size)
                 if horizontal:
                     for ii in range(i, i + size):
                         if board[ii] == '.':
-                            board_counts[ii] = board_counts[ii] + 1
+                            board_counts[ii] = board_counts[ii] + hweight
                 if vertical:
                     for ii in range(i, i + (self.game.width*size), self.game.width):
                         if board[ii] == '.':
-                            board_counts[ii] = board_counts[ii] + 1
+                            board_counts[ii] = board_counts[ii] + vweight
        
         self.logger.debug('Board:\n{0}\nCount:\n{1}'.format(self.print_board(board), self.print_board(board_counts)))
         return board_counts
 
     def _check_boat_location(self, board, i, size):
         if board[i] == '0':
-            return (False, False)
+            return (False, 1, False, 1)
         #check right and down
         horizontal = int((i + size - 1) / self.game.width) == int(i / self.game.width)
         vertical = int((i + ((size - 1) * self.game.width)) / self.game.width) < self.game.length
+        hweight = 1
+        vweight = 1
         if horizontal or vertical:
             for x in range(0, size):
-                if horizontal and board[i + x] == '0':
-                    horizontal = False
-                if vertical and board[i + (x * self.game.width)] == '0':
-                    vertical = False
+                if horizontal:
+                    if board[i + x] == '0':
+                        horizontal = False
+                    elif board[i + x] == 'X':
+                        hweight = hweight + 5
+                if vertical:
+                    if board[i + (x * self.game.width)] == '0':
+                        vertical = False
+                    elif board[i + (x * self.game.width)] == 'X':
+                        vweight = vweight + 5
                 if not horizontal and not vertical:
                     break
 
-        return (horizontal, vertical)
+        return (horizontal, hweight, vertical, vweight)
 
     def _hit(self, player, x, y):
         self.logger.debug('Hit at ({0},{1})'.format(x, y))
